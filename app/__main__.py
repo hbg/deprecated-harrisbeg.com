8a91d08ef9c95ea47b51e2a69c5532a7614d96b9
Harris Beg <legostarwarsguy02@gmail.com>
2018-11-12 15:33:40 -0800

    Fixed Time Zones

--- app/__main__.py
+++ app/__main__.py
@@ -3,23 +3,22 @@ import datetime
 
 app = Flask(__name__)
 pages = ["Home","About","Contact","Projects"]
-hour = int(datetime.datetime.now().hour)
-print(hour)
+
 @app.route('/')
 def home():
-    return render_template("index.html", name=pages[0], date=hour)
+    return render_template("index.html", name=pages[0])
 
 @app.route('/about/')
 def about():
-    return render_template("about.html", name=pages[1], date=hour)
+    return render_template("about.html", name=pages[1])
 
 @app.route('/contact/')
 def contact():
-    return render_template("contact.html", name=pages[2], date=hour)
+    return render_template("contact.html", name=pages[2])
 
 @app.route('/projects/')
 def projects():
-    return render_template("projects.html", name=pages[3], date=hour)
+    return render_template("projects.html", name=pages[3])
 
 if __name__ == '__main__':
     app.run(debug=True)
