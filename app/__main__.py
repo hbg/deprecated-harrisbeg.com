bbbba46f700474c03e4286bffdf69f60878bb355
Harris Beg <legostarwarsguy02@gmail.com>
2018-11-12 15:19:22 -0800

    Final Fix Heroku Theme

--- app/__main__.py
+++ app/__main__.py
@@ -3,23 +3,23 @@ import datetime
 
 app = Flask(__name__)
 pages = ["Home","About","Contact","Projects"]
-date = int(datetime.datetime.now().hour)
-print(date)
+hour = int(datetime.datetime.now().hour)
+print(hour)
 @app.route('/')
 def home():
-    return render_template("index.html", name=pages[0], date=date)
+    return render_template("index.html", name=pages[0], date=hour)
 
 @app.route('/about/')
 def about():
-    return render_template("about.html", name=pages[1], date=date)
+    return render_template("about.html", name=pages[1], date=hour)
 
 @app.route('/contact/')
 def contact():
-    return render_template("contact.html", name=pages[2], date=date)
+    return render_template("contact.html", name=pages[2], date=hour)
 
 @app.route('/projects/')
 def projects():
-    return render_template("projects.html", name=pages[3], date=date)
+    return render_template("projects.html", name=pages[3], date=hour)
 
 if __name__ == '__main__':
     app.run(debug=True)
