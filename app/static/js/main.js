2fefe85e56121fcc459d642fa038ed9b05dc6d47
Harris Beg <legostarwarsguy02@gmail.com>
2018-11-12 17:40:37 -0800

    Basic UI updates

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,4 +1,11 @@
-
+var module = angular.module('harrisBeg',[]);
+module.controller('t', function($scope, $interval) {
+	var interv = function() {
+		$scope.time = Date.now();
+	}
+	interv();
+	$interval(interv, 1000);
+});
 
 $(document).on('ready', function() {
 	new WOW({
@@ -19,23 +26,31 @@ $(document).on('ready', function() {
 		 *  background: linear-gradient(to right, #4A00E0, #8E2DE2);
 		**/
 		$('body').css("background","-webkit-linear-gradient(to right, #ff5f6d, #ffc371)");
-		$('body').css("background","-webkit-linear-gradient(to right, #ff5f6d, #ffc371)");
+		$('body').css("background","linear-gradient(to right, #ff5f6d, #ffc371)");
 		color('#ffd000');
+		$('html').css("background","-webkit-linear-gradient(to right, #ff5f6d, #ffc371)");
+		$('html').css("background","linear-gradient(to right, #ff5f6d, #ffc371)");
+
 	}
 	else if (hr >= 12 && hr < 19) {
 		$('body').css("background","-webkit-linear-gradient(to right, #36d1dc, #5b86e5)");
 
 		$('body').css("background","linear-gradient(to right, #36d1dc, #5b86e5)");
-		color('#00d8ff');
+		$('html').css("background","-webkit-linear-gradient(to right, #36d1dc, #5b86e5)");
 
+		$('html').css("background","linear-gradient(to right, #36d1dc, #5b86e5)");
+		color('#00d8ff');
 	}
 	else {
 		$('body').css("background","-webkit-linear-gradient(to right, #6441a5, #2a0845)");
 
 		$('body').css("background","linear-gradient(to right, #6441a5, #2a0845)");
 		color('#6441a5');
+		$('html').css("background","-webkit-linear-gradient(to right, #6441a5, #2a0845)");
 
+		$('html').css("background","linear-gradient(to right, #6441a5, #2a0845)");
 	}
+	$('.sidenav').sidenav();
 });
 
 function color(c) {
