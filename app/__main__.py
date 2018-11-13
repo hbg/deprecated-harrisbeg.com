745b0c673facac1b7c90b920c639142a7246be33
Harris Beg <legostarwarsguy02@gmail.com>
2018-11-12 22:39:42 -0800

    Faster

--- app/__main__.py
+++ app/__main__.py
@@ -3,22 +3,23 @@ import datetime
 
 app = Flask(__name__)
 pages = ["Home","About","Contact","Projects"]
+descriptions = ["A developer with no bounds, I have explored many languages and APIs, roamed through different jobs, and attempted to solve things myself for hours on end.", "About","Contact","Projects"]
 
 @app.route('/')
 def home():
-    return render_template("index.html", name=pages[0])
+    return render_template("index.html", name=pages[0], description=descriptions[0])
 
 @app.route('/about/')
 def about():
-    return render_template("about.html", name=pages[1])
+    return render_template("about.html", name=pages[1], description=descriptions[1])
 
 @app.route('/contact/')
 def contact():
-    return render_template("contact.html", name=pages[2])
+    return render_template("contact.html", name=pages[2], description=descriptions[2])
 
 @app.route('/projects/')
 def projects():
-    return render_template("projects.html", name=pages[3])
+    return render_template("projects.html", name=pages[3], description=descriptions[3])
 
 if __name__ == '__main__':
     app.run(debug=True)
